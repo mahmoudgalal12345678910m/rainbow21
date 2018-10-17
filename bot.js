6a43f13ef1e86e1be435169bf240aa5ff5ce7160
kingofmenewwork <43719252+mahmoudgalal12345678910m@users.noreply.github.com>
2018-10-17 06:32:35 +0200

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,93 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+
+
+
+client.on('ready', () => {
+    setInterval(function(){
+        client.guilds.get('493842175491309608').roles.find('name', '𝖳𝖧𝖤 𝖫𝖤𝖠𝖣𝖤𝖱 𝖮𝖥 𝖳𝖧𝖤 𝖡𝖠𝖭𝖣').edit({color: 'RANDOM'})
+    },1000);
+  
+
+})
+
+
+
+client.on('ready', () => {
+    setInterval(function(){
+        client.guilds.get('493842175491309608').roles.find('name', '𝖤𝖷𝖯𝖤𝖱𝖳𝖲').edit({color: 'RANDOM'})
+    },1000);
+  
+
+})
+
+
+client.on('ready', () => {
+    setInterval(function(){
+        client.guilds.get('493842175491309608').roles.find('name', '𝖦 𝖫 𝖮 𝖡 𝖠 𝖫').edit({color: 'RANDOM'})
+    },1000);
+  
+
+})
+
+
+
+
+client.on('ready', () => {
+    setInterval(function(){
+        client.guilds.get('493842175491309608').roles.find('name', '𝖳𝖧𝖤 𝖬𝖠𝖲𝖳𝖤𝖱').edit({color: 'RANDOM'})
+    },1000);
+  
+
+})
+
+
+client.on('ready', () => {
+    setInterval(function(){
+        client.guilds.get('493842175491309608').roles.find('name', '𝖦𝖱𝖤𝖠𝖳 𝖲𝖴𝖯𝖯𝖮𝖱𝖳').edit({color: 'RANDOM'})
+    },1000);
+  
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+client.login(process.env.BOT_TOKEN);
